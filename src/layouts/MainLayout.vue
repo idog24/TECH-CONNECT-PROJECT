d5bd6a7287f26dc6be421ea129466f71bfecd216
idog24 <idogflyer@gmail.com>
2024-04-09 21:03:50 -0400

    Initial commit

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,22 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated>
+      <q-toolbar>
+        <q-toolbar-title> Tech Connect </q-toolbar-title>
+        <q-btn flat dense round icon="menu" aria-label="Menu" />
+      </q-toolbar>
+    </q-header>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+import { defineComponent, ref } from "vue";
+
+export default defineComponent({
+  name: "MainLayout",
+});
+</script>
---
+++ src/pages/LoginPage.vue
@@ -0,0 +1,15 @@
+<template>
+  <q-page class="flex flex-center">
+    <q-input filled label="Username" />
+    <q-input filled label="Password" />
+    <q-btn color="primary" label="Login" />
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "IndexPage",
+});
+</script>
