95d6c3ca12c0817a1eadd273f9aad749099976c0
idog24 <idogflyer@gmail.com>
2024-04-11 22:15:17 -0400

    Finished UserProfile and added basic search page

--- frontend/src/layouts/MainLayout.vue
+++ frontend/src/layouts/MainLayout.vue
@@ -3,7 +3,21 @@
     <q-header elevated>
       <q-toolbar>
         <q-toolbar-title> Tech Connect </q-toolbar-title>
-        <q-btn flat dense round icon="menu" aria-label="Menu" />
+        <q-btn flat dense round icon="menu" aria-label="Menu">
+          <q-menu>
+            <q-list style="min-width: 150px">
+              <q-item clickable v-close-popup @click="this.$router.push({ name: 'login' })">
+                <q-item-section>Login</q-item-section>
+              </q-item>
+              <q-item clickable v-close-popup @click="this.$router.push({ name: 'home' })">
+                <q-item-section>My Profile</q-item-section>
+              </q-item>
+              <q-item clickable v-close-popup @click="this.$router.push({ name: 'search' })">
+                <q-item-section>Search</q-item-section>
+              </q-item>
+            </q-list>
+          </q-menu>
+        </q-btn>
       </q-toolbar>
     </q-header>
 
